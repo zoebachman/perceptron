b3f2fa24266a8a30f466b0faa29277204763d7dc
Zoe Bachman <zoe.bachman@gmail.com>
2016-09-26 15:55:32 -0400

    first commit

---
+++ perceptron.py
@@ -0,0 +1,66 @@
+perceptron.py
+
+# set up threshold algorithm
+# create data set of True (1) and False (-1)
+# do algorithm for And, Or, [X_OR] separately
+
+# add in weight bias when summation
+
+
+data = [[T,T], [T,F], [F,T], [F,F]]
+
+
+
+def add_perceptron(data):
+	weights = list(random.randrange(-1, 1)) #how do you decide range?
+	products = []
+	output_expected = True
+	new_weight = [] #a new list?
+
+	for i in data: # each epoch of training
+		products.append(product) = i * weights[i]
+
+
+	for item in products: #summation
+		summation = product[n] + product[n+1] #how to not count multiples
+
+	#threhold algorithm
+	if summation > 0:
+		output = True
+	elif summation < 0:
+		output = False
+
+	#check errors
+	error = output_expected - output
+
+	weight_change = error * data[i]
+
+	new_weight.append(old_weight + weight_change)
+
+def or_perceptron(data):
+	weights = list(random.randrange(-1, 1)) #how do you decide range?
+	products = []
+	output_expected = True
+	new_weight = [] #a new list?
+
+	for i in data: # each epoch of training
+		products.append(product) = i * weights[i]
+
+
+	for item in products: #summation
+		summation = product[n] + product[n+1] #how to not count multiples
+
+	#threhold algorithm
+	if summation > 0:
+		output = True
+	elif summation < 0:
+		output = False
+
+	#check errors
+	error = output_expected - output
+
+	weight_change = error * data[i]
+
+	new_weight.append(old_weight + weight_change)
+
+
