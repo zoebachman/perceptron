c4cd89e24739ad476fa3f0c5e0307d10fb158820
Zoe Bachman <zoe.bachman@gmail.com>
2016-09-29 16:16:36 -0400

    re-structured code, got rid of while loop, separate loop to re-seed weights

--- and_perceptron.py
+++ and_perceptron.py
@@ -1,61 +1,101 @@
 import random
 
 # set up threshold algorithm
-# create data set of True (1) and False (-1)
-# do algorithm for And, Or, [X_OR] separately
-
-# add in weight bias when summation
-
-# where to put weights initially?
-# where do we generate weights so that we can put them back into the algorithm? 
-
 #do you average the outputs to get the final one?
 
-#use a while loop to go through until correct answer is reached?
+# The Procedure:
+
+# Provide the Perceptron with inputs for which there are known outputs.
+# Ask the Perceptron to guess the outputs.
+# Compute the error.
+# Adjust all weights according to error.
+# Return to Step 1 and repeat.
 
 
 T = 1
 F = -1
 bias = 1
-expected_output = T
-
-#input and weights should be 2 lists
-
-data_input = [T, T]
-# data_input = [[T,T], [T,F], [F,T], [F,F]]
 
 
-summation = 0
+data_input = [[T,T], [T,F], [F,T], [F,F]]
+guess = []
+expected_output = [T, F, F, F]
 
-weights = []
+error_list = []
 
-
-def add_perceptron(data, weight): #processor 
-	# weights = list(random.randrange(-1, 1)) #how do you decide range?
-	products = []
-	expected_output = T
-	new_weight = [] #a new list?
-
-	for i in data: # each epoch of training
-		products.append(product) = i * weights[i]
+# new_weights = []
+learning_constant = .01
+# summation = 0 #got this from NoC
 
 
-	for item in products: #summation
-		summation = product[n] + product[n+1] + bias #how to not count multiples
+for i in range(1): #run the perceptron an x number of times
+	for j in range(len(data_input)): #for each index in data_input (so 4 times)
+		
+		weights = []
+		for i in range(3):
+			weights.append(random.uniform(-1, 1))
+
+		#ask the perceptron to guess the outputs
+		summation = (data_input[j][0] * weights[0]) + (data_input[j][1] * weights[1]) + (bias * weights[2])
+
+		#threhold algorithm; calculate output > is this ok? only dealing with ints...will it ever get closer then?
+		if summation > 0: # if summation is a positive number
+			guess = T
+		elif summation < 0: # if summation is a negative number
+			guess = F
+
+		output_value = expected_output[j]
+
+		#if not doing threshold algorithm:
+		# error = output_value - summation
+
+		#compute the error
+		error = output_value - guess
+
+		
+		error_list.append(error) #should be 4, and I want this in a list to refer back to 
+
+		
+
+	print error_list #4 overall errors
+		
+
+	#reset the weights - but, before we try next thing in the list?
+	#shouldn't this happen with each iteration in range rather than each time through the input?
+	#so, does it work to have the first loop run and thennnn do another loop? 
+
+	#define change in weight, referencing error_list
+	
+	for j in range(len(data_input)):
+		delta_weight0 = error_list[i] * data_input[j][0]
+		delta_weight1 = error_list[i] * data_input[j][1]
+		delta_bias_weight = error_list[i] * bias
 
 
+		#create new weights
+		weights[0] = weights[0] + delta_weight0 * learning_constant
+		weights[1] = weights[1] + delta_weight0 * learning_constant
+		weights[2] = weights[2] + delta_bias_weight * learning_constant
 
-	#threhold algorithm; calculate output
-	if summation > 0:
-		output = T
-	elif summation < 0:
-		output = F
+		#how do I seed the new weights into the next round?
+
+
+	# for i in weights: #for each item in weights
+		# new_weights[i] +=  learning_constant * error #append a new weight plus a learning constant, taking into account the error?
+
+
+	
+
 
 	#check errors
-	error = expected_output - output
+	# error = expected_output - output
 
-	weight_change = error * data[i]
+	# weight_change = error * data[i]
 
-	new_weight.append(old_weight + weight_change)
+	# new_weight.append(old_weight + weight_change)
 
-	return new_weight
+	# return new_weight
+
+
+
+#thinking of end goal of weight?
