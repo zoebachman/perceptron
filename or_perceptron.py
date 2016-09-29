f89ab19ea2ede9c152c31bfd3d866aa6be2bd2ff
Zoe Bachman <zoe.bachman@gmail.com>
2016-09-29 18:28:35 -0400

    correct OR gate working

--- or_perceptron.py
+++ or_perceptron.py
@@ -1,59 +1,76 @@
 import random
+import numpy as np
+import matplotlib.pyplot as plt
 
-# set up threshold algorithm
-# create data set of True (1) and False (-1)
-# do algorithm for And, Or, [X_OR] separately
 
-# add in weight bias when summation
+# The Procedure:
 
-# where to put weights initially?
-# where do we generate weights so that we can put them back into the algorithm? 
-
-#do you average the outputs to get the final one?
-
-#use a while loop to go through until correct answer is reached?
+# Provide the Perceptron with inputs for which there are known outputs.
+# Ask the Perceptron to guess the outputs.
+# Compute the error.
+# Adjust all weights according to error.
+# Return to Step 1 and repeat.
 
 
 T = 1
 F = -1
 bias = 1
-expected_output = T
-
-#input and weights should be 2 lists
-
-data_input = [T, T]
-# data_input = [[T,T], [T,F], [F,T], [F,F]]
 
 
-summation = 0
+data_input = [[T,T], [T,F], [F,T], [F,F]]
+
+expected_output = [T, T, T, F]
 
 weights = []
 
-def or_perceptron(data, weight): # processor
-	# weights = list(random.randrange(-1, 1)) #how do you decide range?
-	products = []
-	output_expected = False
+#seed random weights between -1 and 1 to start (floats)
+for i in range(3):
+	weights.append(random.uniform(-1,1))
 
-	new_weight = [] #a new list?
+learning_constant = .1
 
-	for i in data: # each epoch of training
-		products.append(product) = i * weights[i]
+errors_list = []
 
 
-	for item in products: #summation
-		summation = product[n] + product[n+1] + bias #how to not count multiples
+for i in range(100): #run the perceptron an x number of times
+	for j in range(len(data_input)): #for each index in data_input
+			
+		#ask the perceptron to guess the outputs
+		summation = (data_input[j][0] * weights[0]) + (data_input[j][1] * weights[1]) + (bias * weights[2])
 
-	#threhold algorithm
-	if summation > 0:
-		output = True
-	elif summation < 0:
-		output = False
+		#threhold algorithm; calculate output
 
-	#check errors
-	error = output_expected - output
+		if summation > 0: # if summation is a positive number
+			guess = T
+		elif summation < 0: # if summation is a negative number
+			guess = F
 
-	weight_change = error * data[i]
+		output_value = expected_output[j]
+		error = output_value - guess
 
-	new_weight.append(old_weight + weight_change)
 
-	return new_weight
+		#define change in weight
+		delta_weight0 = error * data_input[j][0]
+		delta_weight1 = error * data_input[j][1]
+		delta_bias_weight = error * bias
+
+
+		#create new weights to seed the next pair of inputs and bias
+		weights[0] = weights[0] + delta_weight0 * learning_constant
+		weights[1] = weights[1] + delta_weight1 * learning_constant
+		weights[2] = weights[2] + delta_bias_weight * learning_constant
+
+		errors_list.append(error)
+
+print errors_list
+
+def plotErrors(thing,label): #borrowed from Eve
+    plt.xlabel('Iteration')
+    plt.ylabel(label)
+    plt.plot(thing)
+    plt.show()
+
+
+
+# plotThing(211,errorsWithBias,'error with bias')
+plotErrors(errors_list, 'errors')
